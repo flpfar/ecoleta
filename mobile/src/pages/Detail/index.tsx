a8972c10e4858e4b4b1dafd7d89a6e25b732acf6
Felipe Rosa <flp.far@gmail.com>
2020-06-07 10:11:20 -0300

    Update image_url for points in mobile

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -14,6 +14,7 @@ interface Params {
 interface Data {
   point: {
     image: string;
+    image_url: string;
     name: string;
     email: string;
     whatsapp: string;
@@ -62,7 +63,7 @@ const Detail = () => {
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
         <TouchableOpacity onPress={handleNavigateBack}><Icon name="arrow-left" size={20} color="#34CB79" /></TouchableOpacity>
-        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 
